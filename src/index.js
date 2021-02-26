ab741f191e6fcb5cdecee32bdd213d0e7a72f740
Will Burke <billyburke1012@gmail.com>
2021-02-26 10:06:11 -0500

    minor update

--- src/index.js
+++ src/index.js
@@ -4,23 +4,28 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//we are importing a thing named createStore from the redux node //module
-import { createStore } from 'redux';
+// We are importing a thing *named* createStore
+// from the redux node module.
+import { createStore, combineReducers } from 'redux';
 
 // State: this is your data!
 // Describe what state looks like, as a default value
-
 const defaultState = {
-  balance: 50
+  banking: {
+    amount: 50
+  },
+  investment: {
+    amount: 100
+  }
 };
 
-//Actions: these are your "withdrawal" and "deposit" slips
+// Actions: these are your "withdrawal" and "deposit" slips
 // Rules of actions:
-// 1. they must be an object
+// 1. They must be an object
 // 2. They must have a `type` key (i.e., type: 'withdrawal' or type: 'deposit')
-// (optional third rule) 3. attach and additional info under a 'payload' key
-//    example: payload: { amount: 5 }
-//What kinds of changes would we want to make to state?
+// (optional third rule) 3. attach any additional info under a `payload` key
+//      example: payload: { amount: 5 }
+// What kinds of changes would we want to make to state?
 // {
 //   type: 'withdraw',
 //   payload: {
@@ -31,34 +36,69 @@ const defaultState = {
 // {
 //   type: 'deposit',
 //   payload: {
-//     amount: 10000000
+//     amount: 100000000
 //   }
 // }
-// Write Action Creator Functions that generate those action objects
-const WITHDRAW = 'withdraw';
-const DEPOSIT = 'deposit';
+const BANKING_WITHDRAW = 'banking/withdraw';
+const BANKING_DEPOSIT = 'banking/deposit';
+const INVESTMENT_WITHDRAW = 'investment/withdraw';
+const INVESTMENT_DEPOSIT = 'investment/deposit';
+const TRANSFER = 'transfer';
+const ACCOUNT_BANKING = 'banking';
+const ACCOUNT_INVESTMENT = 'investment';
+// Write Action Creator functions that generate those action objects!!!!
 
-const deposit = (amount) => ({
-  
-    type: 'deposit', //not an arg because typos
+const transfer = (amount, from, to) => (
+  from === to ? { type: ''}  // If `from` and `to` are the same, 
+                             // return an empty type
+  :                          // Otherwise, return an action object
+  {
+    type: TRANSFER,
+    payload: {
+      amount,
+      from,
+      to
+    }
+  }
+);
+
+const bankingDeposit = (amount) => (
+  {
+    type: BANKING_DEPOSIT, // not an arg, because typos!
+    payload: {
+      amount
+    }
+  }  
+);
+
+const bankingWithdraw = (amount) => (
+  {
+    type: BANKING_WITHDRAW,
+    payload: {
+      amount
+    }
+  }
+)
+
+const investmentDeposit = (amount) => (
+  {
+    type: INVESTMENT_DEPOSIT,
+    payload: {
+      amount
+    }
+  }
+);
+const investmentWithdraw = (amount) => (
+  {
+    type: INVESTMENT_WITHDRAW,
     payload: {
       amount
     }
   }
 );
 
-const withdraw = (amount) => ({
-  
-  type: 'withdraw', //not an arg because typos
-  payload: {
-    amount
-  }
-}
-);
-
-
-//Reducer: this calculates how an action changes state
-const bankTeller = (state, action) =>{
+// Reducer: this calculates how an action changes state
+const banking = (state=defaultState.banking, action) => {
   if (!action) return state;
   if (!action.payload) return state;
 
@@ -66,83 +106,83 @@ const bankTeller = (state, action) =>{
     ...state
   };
   // - access your account balance
-  //    it receives a copy of the current state
-  // - see if you are doing a withdraw or deposit
-  //    it receives the action
-    switch (action.type){
-      case DEPOSIT:
-        console.log('depositing!!!!!!')
-        console.log(`they currently have ${newState.balance}`)
-        if (newState.balance >= 500){
-          console.log(`over 500 current balance ${newState.balance}`);
-          newState.balance = newState.balance + (newState.balance / 100)
-        }
-        newState.balance += action.payload.amount;
-        console.log(`they will have ${newState.balance}`)
-       
-        break;
-      case WITHDRAW:
-        console.log('taking money out')
-        let penalty = 0
-        
-        
-        if (newState.balance - action.payload.amount < 0){
-          console.log('balance too low, you are penalized $25')
-          penalty = 25
-        } else {
-          newState.balance -= action.payload.amount;
-          newState.balance -= penalty;
-        }
-        // if (newState.balance < 0) {
-        //   console.log('not enough $$$')
-        // } 
-        break;
-      default:
-        // no change
-        break;
-    }
-    
-  // - update your balance after calculating new value
-  //    it returns the new version of state!!!
-  console.log(newState)
-  return newState;
-  
+  //      it receives a copy of the current state
+  // - see if you're doing a withrawal or deposit
+  //      it receives the action
+  switch (action.type) {
+    case BANKING_DEPOSIT:
+      newState.amount += action.payload.amount;
+      break;
+    case BANKING_WITHDRAW:
+      newState.amount -= action.payload.amount;
+      break;
+    case TRANSFER:
+      if (action.payload.from === ACCOUNT_BANKING) {
+        newState.amount -= action.payload.amount;
+      } else if (action.payload.to === ACCOUNT_BANKING) {
+        newState.amount += action.payload.amount;
+      }
+      break;
+    default:
+      // no change
+      break;
+  }
 
+  // - update your balance after calculating new value
+  //      it returns the new version of state!!!
+  return newState;
 }
 
-// The store: spoiler alert - redux gives you a function to create one
-const store = createStore(bankTeller, defaultState);
-//.subscribe() - you pass it a function, it runs your function when state changes
-// .dispatch() -you pass it an action, it calls the reducer and updates state
-
-
-// becasue we're not using react yet, we'll attach some things to the global window object:
-
-window.store = store;
-window.deposit = deposit;
-window.withdraw = withdraw;
-
-// step two, set up automatic console.log() when state changes
-store.subscribe(() => {
-  console.log('------------state was updated----------------')
-  console.table(store.getState())
+const investment = (state=defaultState.investment, action) => {
+  if (!action) return state;
+  if (!action.payload) return state;
   
-})
+  let newState = {
+    ...state
+  };
 
+  switch (action.type) {
+    case INVESTMENT_DEPOSIT:
+      newState.amount += action.payload.amount;
+      break;
+    case INVESTMENT_WITHDRAW:
+      newState.amount -= action.payload.amount;
+      break;
+    case TRANSFER:
+      if (action.payload.from === ACCOUNT_INVESTMENT) {
+        newState.amount -= action.payload.amount;
+      } else if (action.payload.to === ACCOUNT_INVESTMENT) {
+        newState.amount += action.payload.amount;
+      }
+      break;
+    default:
+      break;
+  }
 
+  return newState;
+}
 
+// The store: spoiler alert - redux gives you a function to create one!
+const rootReducer = combineReducers({
+  banking,
+  investment
+});
+const store = createStore(rootReducer, defaultState);
+// .subscribe() - you pass it a function, it runs your function when state changes
+// .dispatch() - you pass it an action, it calls the reducer and updates state
 
+// Because we're not using React yet, we'll attach some things to the global window object:
+window.store = store;
+window.bankingDeposit = bankingDeposit;
+window.bankingWithdraw = bankingWithdraw;
+window.investmentDeposit = investmentDeposit;
+window.investmentWithdraw = investmentWithdraw;
+window.transfer = transfer;
+window.ACCOUNT_BANKING = ACCOUNT_BANKING;
+window.ACCOUNT_INVESTMENT = ACCOUNT_INVESTMENT;
 
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Step two, set up automatic console.log() when the state changes
+store.subscribe(() => {
+  console.log('-------- state was updated --------');
+  console.table(store.getState());
+});
